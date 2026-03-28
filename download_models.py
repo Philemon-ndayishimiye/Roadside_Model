0e95521910134ea36d6e3c78230693619030dbf3
Philemon-ndayishimiye <philemonndayi@gmail.com>
2026-03-28 17:13:56 +0200

    initialize huggingface

--- download_models.py
+++ download_models.py
@@ -1,22 +1,27 @@
 # download_models.py
 import os
-import gdown
+from huggingface_hub import hf_hub_download
 
 MODELS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models')
-
-DRIVE_FILES = {
-    'RandomForest_fault_model.pkl': '1FM_Ke2IUO1Q7jYxsFN4ZSw5Fqpjszb6v',
-    'le_fault.pkl'                : '1sZy3pBbGbv8kpST5SY3luhGuVQQOsi0I',
-    'fault_classes.json'          : '1b7uRZ5f6k_F2cCHj_A-oRUlQKxAgigqd',
-}
-
 os.makedirs(MODELS_DIR, exist_ok=True)
 
-for filename, file_id in DRIVE_FILES.items():
+REPO_ID = "philemonndayi/Roadaid_model"  # ← replace this
+
+FILES = [
+    'RandomForest_fault_model.pkl',
+    'le_fault.pkl',
+    'fault_classes.json',
+]
+
+for filename in FILES:
     filepath = os.path.join(MODELS_DIR, filename)
     if not os.path.exists(filepath):
         print(f'⏳ Downloading {filename}...')
-        gdown.download(f'https://drive.google.com/uc?id={file_id}', filepath, quiet=False)
+        hf_hub_download(
+            repo_id=REPO_ID,
+            filename=filename,
+            local_dir=MODELS_DIR
+        )
         print(f'✅ {filename} done!')
     else:
         print(f'✅ {filename} already exists')
